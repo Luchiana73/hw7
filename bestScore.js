bc1caa346019a2bee727bc343ce4b4d3e3a723e6
Svetlana <luchiana.sv@gmail.com>
2023-01-31 18:22:41 +0100

    created a new script - player with the best score

---
+++ bestScore.js
@@ -0,0 +1,37 @@
+const players = [
+  { 
+    id: 1, 
+    name: "Ivan", 
+    scorePoints: 4500 
+  },
+  { 
+    id: 2, 
+    name: "Petr", 
+    scorePoints: 3600 
+  },
+  { 
+    id: 3, 
+    name: "Vadim", 
+    scorePoints: 3433 
+  },
+  { 
+    id: 4, 
+    name: "Olga", 
+    scorePoints: 2356 
+  },
+];
+
+let bestScore = [];
+
+for (let player of players) {
+  bestScore.push(player.scorePoints);
+  Math.max(...bestScore);
+};
+
+
+const bestPlayer = players.filter(player => {
+  if (player.scorePoints == Math.max(...bestScore)) {
+    return true
+  }
+})
+console.log(bestPlayer);
