cc7fb010f43635f7804a1ddfff07afd1a89d6229
Svetlana <luchiana.sv@gmail.com>
2023-02-01 18:41:54 +0100

    created a new script - class button

---
+++ button.js
@@ -0,0 +1,30 @@
+class Button {
+  constructor(height, width, type, color) {
+    this.height = height;
+    this.width = width;
+    this.type = type;
+    this.color = color;
+    this.props =
+      this.height + " " + this.width + " " + this.type + " " + this.color;
+  }
+  onClick() {
+    return this.props;
+  }
+}
+let firstButton = new Button(10, 5, "button", "green");
+console.log(`First button properties are ${firstButton.onClick()}`);
+
+function testButton(height, width, type, color) {
+  if (
+    (firstButton.height === 10) &
+    (firstButton.width === 5) &
+    (firstButton.type === "button") &
+    (firstButton.color === "green")
+  ) {
+    console.log(`This button is valid`);
+  } else {
+    console.log(`The properties of this button are invalid`);
+  }
+}
+
+testButton();
