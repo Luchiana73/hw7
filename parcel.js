c6f715f5863f6d107272932a27bfac1821afc848
Svetlana <luchiana.sv@gmail.com>
2023-02-02 11:14:57 +0100

    added modification to the script parcel

--- parcel.js
+++ parcel.js
@@ -3,10 +3,14 @@ let smsCode = "A001DFX0";
 let parcelLocker = [0, 0, 0, 4, 0];
 let lockerCode = "A001DFX0";
 
-if (lockerCode == smsCode) {
+if (smsCode === lockerCode && accountbBalance >= 100) {
   console.log(
     `Вы получили посылку из ячейки ${parcelLocker[3]} и ваш счет составляет: ${
       accountbBalance - 100
     } руб.`
   );
-} 
+} else if (smsCode !== lockerCode) {
+  console.log(`Вы ввели неправильный код`);
+} else if (accountbBalance < 100) {
+  console.log(`На вашем счете недостаточно средств`);
+}
