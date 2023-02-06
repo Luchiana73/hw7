9dc9cdfd341f7fb755384465a79dd1d01d5c46a3
Svetlana <luchiana.sv@gmail.com>
2023-02-06 18:28:14 +0100

    added new modifications to the script button

--- 7.3.button.js
+++ 7.3.button.js
@@ -4,21 +4,25 @@ class Button {
     this.width = width;
     this.type = type;
     this.color = color;
-    this.props = `${this.height}, ${this.width}, ${this.type}, ${this.color}`;
+    this.props = `height: ${this.height}, width: ${this.width}, type: ${this.type}, color: ${this.color}`;
   }
   onClick() {
-    return this.props;
+    console.log(`Button properties are: ${this.props}`);
   }
 }
+
 let firstButton = new Button(10, 5, "button", "green");
-console.log(`First button properties are ${firstButton.onClick()}`);
+let secondButton = new Button(12, 7, "submit", "yellow")
+
+firstButton.onClick();
+secondButton.onClick();
 
 function testButton(height, width, type, color) {
   if (
-    (firstButton.height === 10) &
-    (firstButton.width === 5) &
-    (firstButton.type === "button") &
-    (firstButton.color === "green")
+    (this.height === testButton.height) &&
+    (this.width === testButton.width) &&
+    (this.type === testButton.type) &&
+    (this.color === testButton.color)
   ) {
     console.log(`This button is valid`);
   } else {
@@ -26,4 +30,5 @@ function testButton(height, width, type, color) {
   }
 }
 
-testButton();
+testButton(firstButton);
+testButton(secondButton)
